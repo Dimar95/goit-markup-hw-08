27ef3d24f2947de37a15af6b41678f48ba5b1670
Kornilov <dp310895kdi@gmail.com>
2022-09-24 23:47:23 +0300

    Test 24/09

--- js/mobilmenu.js
+++ js/mobilmenu.js
@@ -2,7 +2,11 @@
     const menuBtnRef = document.querySelector("[date-menu-button]");
     const mobileMenuRef = document.querySelector("[date-menu]");
     menuBtnRef.addEventListener("click", () => {
+        const expanded =
+            menuBtnRef.getAttribute("aria-expanded") === 'true' || false;
+        
         menuBtnRef.classList.toggle("is-open");
+        menuBtnRef.setAttribute("aria-expanded", !expanded);
         mobileMenuRef.classList.toggle("is-open");
     });
 })();
